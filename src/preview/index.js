6bd66c4b4bfd3e078014bd1448d5d564342c81e9
Michael Sievers <michael_sievers@web.de>
2016-08-02 15:30:25 +0200

    WIP

--- src/preview/index.js
+++ src/preview/index.js
@@ -8,30 +8,46 @@ ipcRenderer.on("editor-content", function(event, data) {
   seat_map.setState({seat_map: data})
 })
 
-let SeatMap = React.createClass({
-  getInitialState: function() {
-    return { seat_map: {} }
-  },
+class SeatMap extends React.Component {
+  constructor(props) {
+    super(props);
+    this._onClick = this._onClick.bind(this);
+    this.state = { seat_map: {}, ui: {} }
+  }
 
-  render: function() {
+  render() {
     let t = React.createElement;
 
     return (
-      t("svg", {width: "100%", height: "100%", style: { position: "fixed", top: 0, bottom: 0, left: 0, right: 0 }},
-        (this.state.seat_map.blocks || []).map((block_props) => {
-          return t(Block, block_props)
-        })
+      t("div", {},
+        t("svg", {width: "100%", height: "100%", onClick: this._onClick, style: { position: "fixed", top: 0, bottom: 0, left: 0, right: 0 }},
+          this._blocks().map((block) => { return t(Block, block) })
+        )
       )
     )
   }
-});
+
+  // helpers
+  _blocks() {
+    return this.state.seat_map.blocks || []
+  }
+
+  _onClick(event) {
+    document.title = `x = ${event.nativeEvent.x}, y = ${event.nativeEvent.y}`
+  }
+}
 
 class Block extends React.Component {
   render() {
     let t = React.createElement;
 
     return (
-      t("polygon", { points: this._svg_points() })
+      t("g", {},
+        [
+          t("polygon", { className: "section", points: this._svg_points() })
+        ]
+        .concat((this.props.rows || []).map((row) => { return t(Row, row) }))
+      )
     )
   }
 
@@ -41,70 +57,29 @@ class Block extends React.Component {
   }
 }
 
-let circles = [
-  {cx: 50, cy: 40, r: 40, stroke: "green", strokeWidth: 4, fill: "yellow"},
-  {cx: 50, cy: 140, r: 40, stroke: "blue", strokeWidth: 4, fill: "red"}
-];
+class Row extends React.Component {
+  render() {
+    let t = React.createElement;
+
+    return (
+      t("g", {},
+        (this.props.seats || []).map((seat) => { return t(Seat, seat) })  
+      )
+    )
+  }
+}
+
+class Seat extends React.Component {
+  render() {
+    let t = React.createElement;
+
+    return (
+      t("circle", {cx: this.props.x, cy: this.props.y, r: 4, "data-number": this.props.number})
+    )
+  }
+}
+
+
 
 // http://stackoverflow.com/questions/25791955/react-js-reactcomponent-does-not-offer-setstate
 let seat_map = ReactDOM.render(React.createElement(SeatMap), document.getElementById("content"));
-
-/*
-let seat_map_data = {
-  "name": "Hall of fame",
-  "blocks": [
-    {
-      "name": "Pit left",
-      "points": [
-        {
-          "x": 72.1,
-          "y": 114.2
-        },
-        {
-          "x": 372.1,
-          "y": 114.2
-        },
-        {
-          "x": 372.1,
-          "y": 464.2
-        },
-        {
-          "x": 32.1,
-          "y": 464.2
-        },
-        {
-          "x": 32.1,
-          "y": 164.2
-        }
-      ],
-      "rows": [
-        {
-          "seats": [
-            {
-              "x": 77.09,
-              "y": 134.19,
-              "number": 1
-            },
-            {
-              "x": 89.09,
-              "y": 124.19,
-              "number": 2
-            },
-            {
-              "x": 101.09,
-              "y": 114.19,
-              "number": 3
-            },
-            {
-              "x": 113.09,
-              "y": 104.19,
-              "number": 4
-            }
-          ]
-        }
-      ]
-    }
-  ]
-}
-*/
-//seat_map.setState({seat_map: seat_map_data})
